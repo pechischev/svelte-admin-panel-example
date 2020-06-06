3ffe1231f256fec0093f7a2ba7d8bbe935165d60
pvarentsov <varentsovpavel@gmail.com>
2020-06-07 01:35:04 +0300

    Test deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -20,8 +20,17 @@ ANSIBLE_EXTRA_VARS=\
  docker_registry_user=${DOCKER_REGISTRY_USER}\
  docker_registry_pass=${DOCKER_REGISTRY_PASS}\
 "
+
+echo "HOST_IP ${HOST_IP}"
+echo "DOCKER_IMAGE ${DOCKER_IMAGE}"
+echo "BUILD_VERSION ${BUILD_VERSION}"
+echo "DOCKER_REGISTRY ${DOCKER_REGISTRY}"
+echo "DOCKER_REGISTRY_USER ${DOCKER_REGISTRY_USER}"
+echo "DOCKER_REGISTRY_PASS ${DOCKER_REGISTRY_PASS}"
+cat ./private-key.txt
+
 ansible-playbook \
-  --inventory ./inventory.${ENV} ./playbook.${ENV}.yml \
+  --inventory ./inventory.host ./playbook.${ENV}.yml \
   --private-key ./private-key.txt \
   --user admin \
   --extra-vars "${ANSIBLE_EXTRA_VARS}" \
